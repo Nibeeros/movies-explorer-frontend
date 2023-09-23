d49dbb742c0dc98cbc1e9c502cca883542c74900
Nibeeros <nibeeros@gmail.com>
2023-09-23 17:56:00 +0300

    Диплом рабочая версия без ссылки на пул реквест

--- src/pages/NotFoundPage/NotFoundPage.js
+++ src/pages/NotFoundPage/NotFoundPage.js
@@ -3,12 +3,16 @@ import { Link, useNavigate } from 'react-router-dom';
 const NotFoundPage = (props) => {
   const navigate = useNavigate();
 
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
     <main className={`popup`} aria-label='Модальное окно'>
       <section className='not-found'>
         <h1 className='not-found__title'>404</h1>
         <p className='not-found__subtitle'>Страница не найдена</p>
-        <Link className='not-found__link opacity07' to={navigate(-1)}>
+        <Link className='not-found__link opacity07' onClick={goBack}>
           Назад
         </Link>
       </section>
